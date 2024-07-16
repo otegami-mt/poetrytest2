5bb6c68047abed0657a8c86a153dd9d5939591af
MT <otegami.to.mt@gmail.com>
2024-07-16 05:25:01 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# This is test project for poetry.
+## 1. Usage
+```
+poetry run python poetrytest2
+```
+OR
+```
+poetry shell
+python poetrytest2
+```
---
+++ poetrytest2/__main__.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import numpy as np
+
+def main():
+    df = pd.DataFrame({"num":[1,2,3,4]})
+    print(df)
+    a = np.arange(5)
+    print(a)
+
+if __name__ == "__main__":
+    main()
